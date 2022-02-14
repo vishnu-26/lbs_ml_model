b5c5b7931b48a41b1df33e53d27d64a36d5c8495
vishnu-26 <vishnu26@gmail.com>
2022-02-14 19:41:27 +0530

    second commit

--- app.py
+++ app.py
@@ -1,18 +1,33 @@
 from flask import Flask, render_template, request
+import pandas as pd
 import pickle
+import datetime
 
-model = pickle.load(open('crops_demand.pkl', 'rb'))
+model = pickle.load(open('demo.pkl', 'rb'))
 
 app = Flask(__name__)
 
 @app.route('/predict',methods=['POST'])
 def predict():
 #    print(request.form)
-    year = request.form.get('year')
-    crop = request.form.get('crop')
-    population = request.form.get('population')
-    demand = request.form.get('demand')
+    year = datetime.date.today().year
+    crop = int(request.form.get('crop'))
+#    population = request.form.get('population')
+#    demand = request.form.get('demand')
 
-    result = model.predict([[year, crop, population, demand]])[0]
-    response = {"result": result}
+    data = pd.read_csv('Crops  - Merged Data.csv')
+    print(data[data["crop (paddy-0, wheat-1, jowar-2, bajra-3, arhar-4, onion-5)"]==crop])
+
+    demand_per_person = data[data["crop (paddy-0, wheat-1, jowar-2, bajra-3, arhar-4, onion-5)"]==crop]['demand per person(kg)']
+
+
+    print(demand_per_person)
+    demand_per_person = demand_per_person[crop*5]
+    print(demand_per_person)
+
+    population = model.predict([[year]])[0]
+    
+    total_demand = ((population*demand_per_person)/1000).round(4)
+#    print(po)
+    response = {"total_demand": total_demand}
     return response
